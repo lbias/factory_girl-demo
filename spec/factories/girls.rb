66a08f21efb4f8cd66280127f2cdd29cf6785585
lbias <306508354@qq.com>
2017-06-13 17:17:46 +0800

    rails g scaffold girl

---
+++ spec/factories/girls.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :girl do
+    
+  end
+end
---
+++ test/system/girls_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class GirlsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit girls_url
+  #
+  #   assert_selector "h1", text: "Girl"
+  # end
+end
