d576c7afec1ec4ec9525c1bf678dddf6d0586ee9
lbias <306508354@qq.com>
2017-06-13 17:31:57 +0800

    factory_girl with faker

--- spec/factories/girls.rb
+++ spec/factories/girls.rb
@@ -1,5 +1,19 @@
 FactoryGirl.define do
   factory :girl do
-    
+    # 方式1: 指定 (固定)
+    # name "jack"
+    # phone_number "13945756489"
+    # address "zhangheng Road 666"
+    # single true
+    # beautiful false
+    # age 24
+
+    # 方式2: 使用faker,制作类似真实数据的 假数据 (不固定,产生不同的数据)
+    name {Faker::Name.name }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    address { Faker::Address.street_address }
+    single { Faker::Boolean.boolean }
+    beautiful { Faker::Boolean.boolean }
+    age {Faker::Number.between(20,30) }    
   end
 end
